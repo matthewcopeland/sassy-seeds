c544037eea8bd340d6810ba89ee1886496dad55f
matt <matthewcopeland1@gmail.com>
2012-07-16 03:56:09 -0700

    inital commit.

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source :rubygems
+
+gem 'rake'
+gem 'sinatra', '~> 1.2.3'
+gem 'shotgun', '~> 0.9'
+gem 'haml', '~> 3.1.4'
+
+# Sass & Compass
+gem 'sass', '~> 3.1.12'
+gem 'compass', '~> 0.11.6'
+
+# Sass libraries
+gem 'grid-coordinates', '~> 1.1.4'
---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+# Helpers
+require './lib/render_partial'
+
+# Set Sinatra variables
+set :app_file, __FILE__
+set :root, File.dirname(__FILE__)
+set :views, 'views'
+set :public_folder, 'public'
+set :haml, {:format => :html5} # default Haml format is :xhtml
+
+# Application routes
+get '/' do
+  haml :styleguide, :layout => :'layouts/application'
+end
