9b6d01f0817a83763958eac68ebc31956469627b
matt <matthewcopeland1@gmail.com>
2012-09-14 16:01:07 -0700

    Deleted test js scripts.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,2 @@
 $(document).ready(function() {
-  console.log ("yo dawg");
-	// Open external links in a new window
-	hostname = window.location.hostname
-	$("a[href^=http]")
-	  .not("a[href*='" + hostname + "']")
-	  .addClass('link external')
-	  .attr('target', '_blank');
-
 });
