1d86425d08f31348d3178de3e4e41f516dff35c0
matt <matthewcopeland1@gmail.com>
2012-09-14 15:45:12 -0700

    Reorg of stylesheets from views folder to assets folder. Makes more sense.

--- config.rb
+++ config.rb
@@ -15,9 +15,9 @@ http_javascripts_path = '/javascripts'
 
 # File system locations
 css_dir               = File.join 'public', 'stylesheets'
-sass_dir              = File.join 'views', 'stylesheets'
-images_dir            = File.join 'public', 'images'
-javascripts_dir       = File.join 'public', 'javascripts'
+sass_dir              = File.join 'assets', 'stylesheets'
+images_dir            = File.join 'assets', 'images'
+javascripts_dir       = File.join 'assets', 'javascripts'
 
 # Syntax preference
 preferred_syntax      = :scss
