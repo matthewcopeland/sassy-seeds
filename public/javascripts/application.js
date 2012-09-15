3e4858249b8712f0d820573075c41f1e0a905aa0
matt <matthewcopeland1@gmail.com>
2012-09-14 17:02:25 -0700

    Added basic theme switcher.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 $(document).ready(function() {
-});
+
+  $("#theme-switcher li").click(function(){
+    var stylesheet_name = $(this).attr("rel");
+    $("head").append("<link rel=\"stylesheet\" type=\"text\/css\" href=\"\/stylesheets\/" + stylesheet_name + ".css\" \/>" );
+  });
+
+});//end document ready
