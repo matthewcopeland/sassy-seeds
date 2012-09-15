57956d8f5730e796485a74b8e2e7158cbe1c9a7f
matt <matthewcopeland1@gmail.com>
2012-09-14 20:23:02 -0700

    Factored button-bg formulas out into reusable mixins. Added theme switch selected state.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 
-  $("#theme-switcher li").click(function(){
+  // theme switcher
+  var theme_switch = $("#theme-switcher li");
+  theme_switch.click(function(){
     var stylesheet_name = $(this).attr("rel");
     $("head").append("<link rel=\"stylesheet\" type=\"text\/css\" href=\"\/stylesheets\/" + stylesheet_name + ".css\" \/>" );
+    $(".selected").removeClass("selected");
+    $(this).addClass("selected");
   });
 
   // active events in mobile webkit.
