395b29a4695012e46dc65045ca26508e1daad592
matt <matthewcopeland1@gmail.com>
2012-10-08 13:14:30 -0500

    Modified theme switcher's nasty-js. Now slightly less nasty.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,11 +2,15 @@ $(document).ready(function() {
 
   // theme switcher
   var theme_switch = $("#theme-switcher li");
-  theme_switch.click(function(){
+  theme_switch.click(function() {
     var stylesheet_name = $(this).attr("rel");
+    var oldStyleSheet = $('link[rel=stylesheet]');
+
     $("head").append("<link rel=\"stylesheet\" type=\"text\/css\" href=\"\/stylesheets\/" + stylesheet_name + ".css\" \/>" );
     $(".selected").removeClass("selected");
     $(this).addClass("selected");
+
+    oldStyleSheet.remove();
   });
 
   // active events in mobile webkit.
