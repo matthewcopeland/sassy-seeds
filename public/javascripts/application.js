b9285c46b3f3bc564e67eb9c733eee0df911cfd0
Paul Sullivan <paulster23@gmail.com>
2012-10-19 11:13:58 -0400

    Paul | Updated theme switcher, removed hard-coded time delays ; Setting cookie when theme is switched;  reading cookie to set theme and on state of theme switcher buttons after page loads (need to make this less wonky so theme and button are set as the page is loading, not after).

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,51 @@
 $(document).ready(function() {
-
   // theme switcher
+  checkCookieSetStyle();
   var theme_switch = $("#theme-switcher li");
   theme_switch.click(function() {
-    var stylesheet_name = $(this).attr("rel");
-    var oldStyleSheet = $('link[rel=stylesheet]');
-    $('html').fadeOut(500, function(){
-      oldStyleSheet.delay(500).remove();
-    }).delay(500).fadeIn(500);
-
-    $("head").after("<link rel=\"stylesheet\" type=\"text\/css\" href=\"\/stylesheets\/" + stylesheet_name + ".css\" \/>");
-    $(".selected").removeClass("selected");
-    $(this).addClass("selected");
+	var stylesheet_name = $(this).attr("rel");
+	$(".selected").removeClass("selected");
+	$(this).addClass("selected");
+	setStyleCookie("style_name", stylesheet_name, 1);
+	$('html').fadeOut(300, function(){
+      $("link").attr("href", "/stylesheets/" + stylesheet_name + ".css");
+    }).fadeIn(150);
   });
-
   // active events in mobile webkit.
   document.addEventListener("touchstart", function () {}, false);
+});
 
-});//end document ready
+// cookies
+function setStyleCookie(c_name,value,exdays){
+  var exdate=new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+  document.cookie=c_name + "=" + c_value;
+ }
+function checkCookieSetStyle(){
+  var style_name=getStyleCookie("style_name");
+    if (style_name!=null && style_name!="")
+    {
+	  $("link").attr("href", "/stylesheets/" + style_name + ".css");
+	  $(".selected").removeClass("selected");
+	  $('#theme-switcher li[rel='+ style_name + ']').addClass("selected");	
+    }
+	else 
+	{
+		$('#theme-switcher li:first').addClass("selected");
+	}
+  }
+function getStyleCookie(c_name){
+  var i,x,y,ARRcookies=document.cookie.split(";");
+  for (i=0;i<ARRcookies.length;i++)
+  {
+    x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+    y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+    x=x.replace(/^\s+|\s+$/g,"");
+    if (x==c_name)
+      {
+      return unescape(y);
+      }
+    }
+  }
+//end document ready
